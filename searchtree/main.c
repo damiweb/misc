407f2515ed4d6ad6b14341047bd6c277e19c2137
Daniel Miksa (portable) <miksa.daniel.94@gmail.com>
2015-04-29 09:54:01 +0200

    update 0.01

--- searchtree/main.c
+++ searchtree/main.c
@@ -1,18 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define MAX_INPUT 64
-
-
-typedef struct Node
-{
-	char* name;
-	char* surname;
-	char* phoneNumber;
-	Node* leftChild;
-	Node* rightChild;
-} Node;
+#include "header.h"
 
 /* To simplify, program will only support standard alphabetic without acute's or other diacritic characters */
 
@@ -34,7 +20,7 @@ char* get_string(){
 	strncpy(fname, inbuffer, (len-1));
 	fname[len-1] = '\0';
 	/*fputs(fname, stdout);*/
-	free(inbuffer)
+	free(inbuffer);
 	return fname;
 }
 
@@ -45,17 +31,17 @@ int checkSurnameBoS(Node* child, Node* parent, int sign, char* alphaLC, char* al
 	int i;
 	int isParChar = 1;
 	int isChildChar = 1;
-	if (child.surname[sign] == '/0') isChildChar = 0;; /* it means that child has no more signs in array */
-	if (parent.surname[sign] == '/0') isParChar = 0;
+	if (child->surname[sign] == '\0') isChildChar = 0;; /* it means that child has no more signs in array */
+	if (parent->surname[sign] == '\0') isParChar = 0;
 	if(isChildChar < isParChar) return 2; /* it means right child */
 	if(isChildChar < isParChar) return 1; /* it means left child */
 	if(isChildChar == 0 && isParChar == 0) return 0; /* it means EQUIVALENT SURNAMES*/
 	for (i = 0; alphaLC[i] != '\0'; ++i)
 	{
-		if(alphaLC[i] == parent.surname[sign]) parentPos = i;
-		if(alphaUC[i] == parent.surname[sign]) parentPos = i;
-		if(alphaLC[i] == child.surname[sign]) childPos = i;
-		if(alphaUC[i] == child.surname[sign]) childPos = i;
+		if(alphaLC[i] == parent->surname[sign]) parentPos = i;
+		if(alphaUC[i] == parent->surname[sign]) parentPos = i;
+		if(alphaLC[i] == child->surname[sign]) childPos = i;
+		if(alphaUC[i] == child->surname[sign]) childPos = i;
 		if((parentPos != -1) && (childPos != -1)) break;
 	}
 	if(childPos > parentPos) return 2; /* it means right child */
@@ -71,20 +57,53 @@ Node* create()
 	char* name = get_string();
 	printf("\nType surname:\n");
 	char* sname = get_string();
-	printf("\nType name: \n");
+	printf("\nType phone number: \n");
 	char* phnumber = get_string();
 	Node* new_contact = (Node*) malloc(sizeof(Node));
-	new_contact.name = name;
-	new_contact.surname = sname;
-	new_contact.phoneNumber = phnumber;
+	new_contact->name = name;
+	new_contact->surname = sname;
+	new_contact->phoneNumber = phnumber;
+	new_contact->leftChild = NULL;
+	new_contact->rightChild = NULL;
 	return new_contact;
 }
 
+void inOrder(Node* origin)
+{
+	if(origin->leftChild != NULL) inOrder(origin->leftChild);
+	else{
+		printf("\n");
+		//fputs(origin->name, stdout);
+		printf("\n");
+		// fputs(origin->surname, stdout);
+		printf("\n");
+		// ``fputs(origin->phoneNumber, stdout);
+		printf("\n");
+		inOrder(origin->rightChild);
+	}
+}
+
+
 main()
 {
-	Node* origin = NULL;
-	Node* c1 = create();
+	Node* origin = create();
 	Node* c2 = create();
-	int insert = checkSurnameBoS(c2, c1, 1, alphaLC, alphaUC);
+	int insert = checkSurnameBoS(c2, origin, 1, alphaLC, alphaUC);
+	if(insert == 1) origin->leftChild = c2;
+	if(insert == 2) origin->rightChild = c2;
 	printf("\nFunkcja zwróciła: %d\n", insert);
+	fputs(origin->surname, stdout);
+	printf("\n");
+	fputs(origin->name, stdout);
+	printf("\n");
+	fputs(origin->phoneNumber, stdout);
+	printf("\n");
+	// inOrder(origin);
+	// if(origin->rightChild != NULL) 
+	// {
+	// 	Node* lc= origin->rightChild;
+	// 	printf("%d\n", lc);
+	// }
+
+
 }
